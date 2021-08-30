6bb7d1c98c525ef9596452021e61ed8a7b6b25f8
hinhnxth2009027 <nguyenhjnh2002@gmail.com>
2021-08-30 14:04:40 +0700

    init project

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\MicrosoftController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/admin/sign-in',[MicrosoftController::class,'ms_login'])->name('ms_login');
+Route::get('/callback',[MicrosoftController::class,'callback'])->name('show_sign_in_view');
